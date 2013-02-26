12adb31ef97d8c01b9f605fea11f74ce19782e55
Guido Bellomo <guido.bellomo@gmail.com>
2013-02-26 11:08:34 +0100

    check task

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 var clc = require('cli-color');
 var fs = require('fs');
 var parseString = require('xml2js').parseString;
-var jshint = require(__dirname+'/node_modules/jackal/node_modules/jshint/packages/jshint/jshint.js').JSHINT;
 
-var tirunnerVersion = '0.2.4';
+
+var tirunnerVersion = '0.2.5';
 
 
 
 var _tiapp = null;
+var _globalLineCount = 0;
 
 module.exports = function(jake,desc,task,complete,fail,file,namespace,appPath) {
-	
+
+
+	var jshint = require(appPath+'/node_modules/tirunner/node_modules/jshint/packages/jshint/jshint.js').JSHINT;	
 	
 	desc('Parse the tiapp.xml file');
 	task('tiapp',{async: true},function() {
@@ -31,6 +34,127 @@ module.exports = function(jake,desc,task,complete,fail,file,namespace,appPath) {
 		    
 		    complete();
 		});
+		
+	});
+
+	/**
+	* @method getFileName
+	* Extract file name from path
+	* @param {String} path
+	* @return {String}
+	*/
+	function getFileName(js) {
+		
+		var a = js.split('/');
+		
+		if (a.length > 0)
+			return a[a.length-1];
+		else return null;
+		
+	}	
+
+	function processLint(strFile) {
+		
+		// open file
+		var data = fs.readFileSync(strFile);
+	
+		// pass to JSHint
+		var result = jshint(data.toString());
+		result = jshint.data();
+	
+		var nChar = data.toString().match(/[\n]/g);	
+		_globalLineCount += nChar != null ? nChar.length : 0;
+		
+		// show errors
+		if (result.errors == null || result.errors.length == 0) {
+			console.log('Parsing: '+clc.cyan(getFileName(strFile))+' .'+'.. '+clc.green('OK'));
+			}
+		else {
+	
+	
+			console.log('----------------------------------');
+			console.log('Parsing: '+clc.red(strFile));
+			console.log('');
+			result.errors.forEach(function(hint) {
+				if (hint != null) {
+				
+				if (hint.line != null) {
+					console.log(clc.green('Line '+hint.line+':'+hint.character)+' '+clc.yellow(hint.reason));
+					}
+					if (hint.evidence)
+						console.log(hint.evidence.replace(/\t/g,''));
+					}
+				});
+			}	
+		
+		// show globals
+		if (result.implieds != null && result.implieds.length != 0) {
+			console.log(clc.yellow('Implied globals:'));
+			result.implieds.forEach(function(value) {	
+				console.log(value.name+': '+clc.green(value.line.toString()));
+				});
+			}
+			
+		// check unused
+		if (result.unused != null && result.unused.length != 0)	{
+			console.log(clc.yellow('Unused variables:'));
+			result.unused.forEach(function(value) {	
+				console.log(clc.green('Line '+value.line)+' '+value.name+' in '+clc.yellow(value['function'].replace(/"/g,'')+'()'));			
+				});
+			}
+		
+		return (result.unused == null || result.unused.length == 0) 
+			&& (result.implieds == null || result.implieds.length == 0)
+			&& (result.errors == null || result.errors.length == 0);
+					
+		}
+
+	
+	desc('Check JavaScript files with JSHint');
+	task('check',function(check_file) {
+
+		console.log('Check javaScript files with JSHint');
+		console.log('');
+		console.log('Suggested JSHint headers:');
+		var code = clc.xterm(246);		
+		console.log('  '+code('/*jshint laxbreak: true,eqnull: true,browser: true,jquery: true,devel: true,regexdash: true,multistr: true, white: false */'));
+		console.log('  '+code('/*global define: true, require: true, module: true, Ti: true, L: true, Titanium: true, Class: true, App: true, exports: true */'));
+		console.log('');
+		
+		// open jsduck files
+		var jsduck = appPath+'/jsduck/jsduck.json';		
+		if (!fs.existsSync(jsduck)) {
+			fail(clc.red('JsDuck files not present. ')+'('+jsduck+')');
+		}
+		var jsduck_content = fs.readFileSync(jsduck,'utf-8');
+
+		var jsduck_json = null;
+		try {
+		jsduck_json = JSON.parse(jsduck_content);
+		} catch(e) {
+			console.log(clc.red('Error parsing JsDuck file.')+'('+e.toString()+')');
+			complete();
+			return false;
+		}
+				
+		// reset
+		_globalLineCount = 0;
+		files = [];
+		jsduck_json['--'].forEach(function(item) {
+			if (check_file == null || check_file == '' || item.indexOf(check_file) !== -1) {			
+				files.push(appPath+'/jsduck/'+item);
+			}
+		});
+		
+		// check each javascript file
+		files.forEach(function(js) {
+			if (!processLint(js))
+				process.exit();	
+			});
+				
+		console.log(clc.green('Check completed! Congrats no errors'));
+		console.log(clc.green('Your LoC is:')+' '+_globalLineCount);
+		console.log('');
 		
 	});
 	
